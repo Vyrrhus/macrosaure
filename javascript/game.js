cc8b6de481bf1506862d87b2ef6d0c00cf4b6628
Vyrrhus <baptistedesoubrie@gmail.com>
2018-07-26 21:04:17 +0200

    Add events

--- javascript/game.js
+++ javascript/game.js
@@ -1,9 +1,39 @@
-// GAME
-var GAME_ON = false;
+var LOCK = false;
 GAME.init();
 
 document.addEventListener('click', function(event) {
-    if (GAME_ON) {return;}
-    GAME.start();
-    GAME_ON=true;
+    if (!LOCK) {
+            GAME.start();
+            LOCK = true;
+        } else {
+            if (!PLAYER.JUMP.STATUS) {
+                PLAYER.switch_status();
+            }
+        }
+    if (GAME.GAME_OVER) {
+        GAME.restart();
+    }
+})
+
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 32) {
+        if (!LOCK) {
+            GAME.start();
+            LOCK = true;
+        } else {
+            if (!PLAYER.JUMP.STATUS) {
+                PLAYER.switch_status();
+            }
+        }
+        if (GAME.GAME_OVER) {
+            GAME.restart();
+        }
+    }
+    if (event.keyCode == 84) {
+        GAME.toggle_hitbox();
+    }
+    if (event.keyCode == 27 || event.keyCode == 80) {
+        GAME.PAUSE = !GAME.PAUSE;
+        GAME.start();
+    }
 })
