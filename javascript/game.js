4e9584b6059ef7409cadc1f444dd052166a4d906
Vyrrhus <baptistedesoubrie@gmail.com>
2018-08-08 12:17:38 +0200

    Add touch event

--- javascript/game.js
+++ javascript/game.js
@@ -1,65 +1,65 @@
-/* We need to fix all those events to declare them in config.js file.
-    + Resize function to try
-    + Focus & blur events
-*/
-
+// LOCK to prevent multiple start (click & keyboard & touch)
 var LOCK = false;
-GAME.init();
+var TOUCH = false;
+
+// EVENTS
+
+window.onload = function() {
+	set_screen();
+	GAME.init();
+};
+
+window.addEventListener('resize', set_screen, false);
+
+document.addEventListener('touchstart', function(event) {
+	if (!LOCK) {
+		start();
+	} else {
+		jump();
+	}
+	TOUCH = true;
+});
 
 document.addEventListener('mouseup', function(event) {
+	if (TOUCH) {
+		TOUCH = false;
+		return
+	}
     if (!LOCK) {
-            GAME.start();
-            LOCK = true;
-        } else {
-            if (!PLAYER.JUMP.STATUS) {
-                PLAYER.switch_status();
-            }
-        }
-    if (GAME.GAME_OVER) {
-            GAME.reset();
-        }
-})
+		start();
+	} else {
+		jump();
+	}
+});
 
 document.addEventListener('keydown', function(event) {
-    if (event.keyCode == 32 || event.jeyCode == 38) {
+    if (event.keyCode == 32 || event.keyCode == 38) {
         if (!LOCK) {
-            GAME.start();
-            LOCK = true;
+            start();
         } else {
-            if (!PLAYER.JUMP.STATUS) {
-                PLAYER.switch_status();
-            }
+            jump();
         }
     }
     if (event.keyCode == 84) {
         GAME.toggle_hitbox();
     }
-    if (event.keyCode == 27 || event.keyCode == 80) {
-        GAME.PAUSE = !GAME.PAUSE;
-        GAME.start();
-    }
-    
-    if (event.keyCode == 16) {
-//        resize();
-    }
-})
+});
 
-function resize() {
-    var height = document.getElementById('body').offsetHeight;
-    var scale = Math.trunc(height/200);
-    HEIGHT *= scale;
-    WIDTH *= scale;
-    resizeCanvas(CANVAS.BACKGROUND, CANVAS.MACRON, CANVAS.OBSTACLE, CANVAS.TEXT);
-    function resizeCanvas() {
-        [].forEach.call(arguments, function(element) {
-            element.height = HEIGHT;
-            element.width = WIDTH;
-        });
-    }
+
+
+// FUNCTIONS
+function start() {
+	GAME.start();
+	LOCK = true;
+}
+
+function jump() {
+	if (!PLAYER.JUMP.STATUS) {
+		PLAYER.switch_status();
+	}
 }
 
 function set_screen() {
-	console.log(`w: ${window.innerWidth} ; h: ${window.innerHeight}`);
 	if ((window.innerWidth - window.innerWidth%WIDTH_TO_HEIGHT_RATIO) / WIDTH_TO_HEIGHT_RATIO <= window.innerHeight) {
 		var new_width = window.innerWidth - window.innerWidth%WIDTH_TO_HEIGHT_RATIO;
 		var new_height = new_width / WIDTH_TO_HEIGHT_RATIO;
@@ -80,32 +80,3 @@ function set_screen() {
 		CANVAS[element].style.marginBottom = top_margin+"px";
 	}
 }
-window.addEventListener('resize', set_screen, false);
-window.onload = set_screen();
-
-
-//window.onload = function() {
-//	resize();
-//	window.addEventListener('resize', resize, false);
-//	function resize() {
-//		WIDTH  = document.getElementById('body').offsetWidth;
-//		HEIGHT = document.getElementById('body').offsetHeight;
-//		CENTER = [Math.floor (WIDTH/2),
-//				  Math.floor(HEIGHT/2)];
-//		resizeCanvas(BACKGROUND,ORBIT,ANIMATION,TEXT,CONTROL);
-//		function resizeCanvas() {
-//			[].forEach.call(arguments, function(element) {
-//				element.width  = WIDTH;
-//				element.height = HEIGHT;
-//			});
-//		}
-//		// Function for backgrounds redraw at each resize
-//		set_BACKGROUND();
-//		FOCUS.draw();
-//		PX.scale(20,30);
-//		BUTTON.draw_ORBIT.draw();
-//		BUTTON.draw_INFO.draw();
-//		BUTTON.draw_TIMELINE.size = 40 + WIDTH/100;
-//		BUTTON.draw_TIMELINE.draw();
-//	}
-//}
